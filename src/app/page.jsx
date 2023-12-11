edd1d43df37a8d9c7077af7e63ffad47be7545bc
koko655 <kokoh2370@gmail.com>
2023-12-11 20:05:35 +0200

    new commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,15 @@
 import Table from "@/components/Table";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Home = () => {
+  const router = useRouter()
+  useEffect(() => {
+    if(localStorage.length === 0){
+      router.push('/login')
+    }
+  }, [])
+  
   return (
     <main className="w-screen h-screen flex justify-center items-center">
       <Table />
